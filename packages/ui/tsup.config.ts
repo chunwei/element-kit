f9b2974d8ae632accd859cc4853b5f56c03f4f96
luchunwei <luchunwei@gmail.com>
2024-11-19 18:55:03 +0800

    no sourcemap

--- packages/ui/tsup.config.ts
+++ packages/ui/tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'tsup'
 export default defineConfig({
   dts: true, // 生成类型定义文件
   minify: true, // 压缩输出
-  sourcemap: true, // 生成源映射
+  sourcemap: false, // 生成源映射
   treeshake: true, // 去除未使用的代码
   splitting: true, // 拆分输出为多个文件
   clean: true, // 在构建前清理输出目录
