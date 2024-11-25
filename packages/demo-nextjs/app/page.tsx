c9517e398d2b26b47e526de2156651dce5cfc488
luchunwei <luchunwei@gmail.com>
2024-11-25 23:25:07 +0800

    @element-kit/ui currently only support client side

--- packages/demo-nextjs/app/page.tsx
+++ packages/demo-nextjs/app/page.tsx
@@ -9,8 +9,22 @@ import {
   ThemeCustomizer,
   ConnectKitButton
 } from '@element-kit/ui'
+import dynamic from 'next/dynamic'
+import { useEffect, useState } from 'react'
+
+// const ElementKitProvider = dynamic(
+//   () => import('@element-kit/ui').then((mod) => mod.ElementKitProvider),
+//   { ssr: false }
+// )
 
 export default function Home() {
+  const [isClient, setIsClient] = useState(false)
+
+  useEffect(() => {
+    setIsClient(true)
+  }, [])
+
+  if (!isClient) return null // 或者返回一个加载占位符
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
@@ -32,6 +46,7 @@ export default function Home() {
             <div className="logos-bar w-full flex justify-between">
               <div className="flex space-x-4 items-center">
                 <a href="https://element.market" target="_blank">
+                  {/*  eslint-disable-next-line @next/next/no-img-element */}
                   <img
                     src="https://static.element.bid/resource/images/favicon-180.png"
                     className="logo element"
