636303ebb19e4a3ee35b47b08ee279a73b1ab17d
luchunwei <luchunwei@gmail.com>
2024-11-26 12:35:01 +0800

    去掉Powerby前面的小图标

--- packages/ui/src/components/PowerdBy/index.tsx
+++ packages/ui/src/components/PowerdBy/index.tsx
@@ -1,20 +1,20 @@
-import {Lock} from 'lucide-react'
-export function PoweredBy(){
-    return (
-      <div className="text-xs  flex items-center justify-center gap-2 -mb-4">
-        <Lock size={8} />
-        <span className="text-muted-foreground">Powered by </span>
-        <img
-          className="w-3 h-3"
-          src="https://static.element.bid/resource/images/favicon-180.png"
-        />
-        <a
-          className="hover:text-primary"
-          href="https://element.market/"
-          target="_blank"
-        >
-          Element
-        </a>
-      </div>
-    )
+// import {Lock} from 'lucide-react'
+export function PoweredBy() {
+  return (
+    <div className="text-xs  flex items-center justify-center gap-2 -mb-4">
+      {/* <Lock size={8} /> */}
+      <span className="text-muted-foreground">Powered by </span>
+      <img
+        className="w-3 h-3"
+        src="https://static.element.bid/resource/images/favicon-180.png"
+      />
+      <a
+        className="hover:text-primary"
+        href="https://element.market/"
+        target="_blank"
+      >
+        Element
+      </a>
+    </div>
+  )
 }
