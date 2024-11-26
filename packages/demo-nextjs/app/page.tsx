0a69bf5ebe1ab71f5271db881f0e0792532987df
luchunwei <luchunwei@gmail.com>
2024-11-26 11:18:10 +0800

    去掉demo-nextjs中localstorage ssr兼容处理

--- packages/demo-nextjs/app/page.tsx
+++ packages/demo-nextjs/app/page.tsx
@@ -9,8 +9,8 @@ import {
   ThemeCustomizer,
   ConnectKitButton
 } from '@element-kit/ui'
-import dynamic from 'next/dynamic'
-import { useEffect, useState } from 'react'
+// import dynamic from 'next/dynamic'
+// import { useEffect, useState } from 'react'
 
 // const ElementKitProvider = dynamic(
 //   () => import('@element-kit/ui').then((mod) => mod.ElementKitProvider),
@@ -18,13 +18,13 @@ import { useEffect, useState } from 'react'
 // )
 
 export default function Home() {
-  const [isClient, setIsClient] = useState(false)
+  // const [isClient, setIsClient] = useState(false)
 
-  useEffect(() => {
-    setIsClient(true)
-  }, [])
+  // useEffect(() => {
+  //   setIsClient(true)
+  // }, [])
 
-  if (!isClient) return null // 或者返回一个加载占位符
+  // if (!isClient) return null // 或者返回一个加载占位符
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center p-8 pb-12 gap-8 sm:px-44 sm:py-8 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
